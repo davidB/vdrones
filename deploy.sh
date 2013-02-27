e6241bd2ee7f8f052a6616e97b0a34b8fef8f011
David Bernard <david.bernard.31@gmail.com>
2013-02-27 21:08:51 +0100

    update deploy.sh to add browser/dart.js

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,14 @@
-appcfg.sh update web
+cd web
+dart2js --minify --output-type=js -oindex.dart.js index.dart
+cd ..
+mkdir -p target
+cat >filter <<EOF
++ packages
++ packages/browser
++ packages/browser/dart.js
+- packages/*
+EOF
+#rsync -av --copy-links  '--include=packages/browser/dart.js' '--exclude=packages' web target 
+rsync -av --copy-links  --filter="merge filter" web target 
+appcfg.sh update target/web
+
