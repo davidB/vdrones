8d4571b13b62bdfe3b13a1c206ed355d1c69a0a7
David Bernard <david.bernard.31@gmail.com>
2013-03-17 21:17:19 +0100

    deploy: deploy less files on GAE

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,20 @@
-cd web
-dart2js --minify --output-type=js -oindex.dart.js index.dart
-cd ..
 mkdir -p target
 cat >filter <<EOF
-+ packages
-+ packages/browser
-+ packages/browser/dart.js
-- packages/*
++ web/packages
++ web/packages/browser
++ web/packages/browser/dart.js
+- web/packages/browser/*
+- web/packages/*
+- packages
+- index.dart*
+- _lib
 EOF
-#rsync -av --copy-links  '--include=packages/browser/dart.js' '--exclude=packages' web target 
-rsync -av --copy-links  --filter="merge filter" web target 
-appcfg.sh update target/web
+#rsync -av --copy-links  '--include=packages/browser/dart.js' '--exclude=packages' web target
+rsync -av --copy-links  --filter="merge filter" web target
+cd web
+dart2js --minify --output-type=js -o../target/web/index.dart.js index.dart
+#dart2js --minify --output-type=dart -o../target/web/index.dart index.dart
+cd ..
+
+appcfg.sh  --use_java7 update target/web
 
