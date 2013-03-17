8a2e631dc2ce971fd586cea5c5c2bcd983043ba6
David Bernard <david.bernard.31@gmail.com>
2013-03-17 17:09:36 +0100

    fix missing js

---
+++ web/_models/drone01.js
@@ -0,0 +1,57 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.65 Exporter",
+		"vertices"      : 6,
+		"faces"         : 5,
+		"normals"       : 5,
+		"colors"        : 0,
+		"uvs"           : [],
+		"materials"     : 1,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "Material",
+		"blending" : "NormalBlending",
+		"colorAmbient" : [0.6400000190734865, 0.41904841093791134, 0.22387421464732604],
+		"colorDiffuse" : [0.6400000190734865, 0.41904841093791134, 0.22387421464732604],
+		"colorSpecular" : [0.0, 0.0, 0.0],
+		"depthTest" : true,
+		"depthWrite" : true,
+		"shading" : "Lambert",
+		"specularCoef" : 50,
+		"transparency" : 1.0,
+		"transparent" : false,
+		"vertexColors" : false
+	}],
+
+	"vertices" : [3.01528,0.0517714,0.652044,-1.02258,-0.94273,0.891053,-1.02258,1.05727,0.891053,-0.177937,0.0572696,1.36519,-1.02258,-0.94273,0.891053,-1.02258,1.05727,0.891053],
+
+	"morphTargets" : [],
+
+	"normals" : [0.098575,-0.000214,-0.995117,-0.622272,-0.77636,-0.10007,-0.62157,0.77691,-0.10007,-0.002106,0.000702,0.999969,-0.059084,0,-0.99823],
+
+	"colors" : [],
+
+	"uvs" : [],
+
+	"faces" : [34,0,1,2,0,0,1,2,34,1,3,2,0,1,3,2,34,0,2,3,0,0,2,3,34,0,3,1,0,0,3,1,34,0,4,5,0,0,4,4],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+
+
+}
