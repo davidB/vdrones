c58354faabd135695f32d149cc2350e6e2b0ec80
David Bernard <david.bernard.31@gmail.com>
2013-04-01 18:02:41 +0200

    disable build.dart, cause multi rebuild, (TODO refactor the build process)

--- build.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'package:web_ui/component_build.dart';
-
-// Ref: http://www.dartlang.org/articles/dart-web-components/tools.html
-main() {
-  build(new Options().arguments, ['web/tests_web_ui.html', 'web/index.html', 'web/index0.html']);
-}
