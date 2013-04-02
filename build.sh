31cc30820645d643c49afea0cde7ec2288352875
David Bernard <david.bernard.31@gmail.com>
2013-04-02 13:33:42 +0200

    fix on build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
+#! /bin/zsh
+
 mkdir -p target
 ln -sf ../packages target/packages
 
 #dart_analyzer --work target/dart-work --package-root=packages/ --metrics --fatal-type-errors --incremental --enable_type_checks --dart-sdk "$DART_SDK" web/index.dart
-dart_analyzer --work target/dart-work --package-root=packages/ --metrics --enable_type_checks --dart-sdk "$DART_SDK" web/index.dart
+dart_analyzer --work target/dart-work --package-root=packages/ --metrics --incremental --enable_type_checks --dart-sdk "$DART_SDK" web/index.dart
 
 
 cat >filter <<EOF
@@ -36,8 +38,9 @@ if [ "x$1" = "x--deploy" ] ; then
 
   dart2js --package-root=packages/ _index0.html_bootstrap.dart -o_index0.html_bootstrap.dart.js
 
-  rm packages
-  rm **/packages
+  rm -Rf packages **/packages
+  rm **/*.dart **/*.dart.map *.dart *.dart.map
+  
   mkdir packages
   cp -R ../../packages/browser packages
 
@@ -49,7 +52,22 @@ if [ "x$1" = "x--deploy" ] ; then
 :s#.dart"#.dart.js"##g
 :x
 EOF
-  rm **/*.dart **/*.dart.map *.dart *.dart.map
+#TODO insert before last </body>
+cat >index.html <<EOF
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-18659445-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  </script>
+EOF
 
 fi
 
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#! /bin/zsh
+
 rm -Rf target
 ./build.sh --deploy
 appcfg.sh  --use_java7 update target/web
