23e94f71accb19c56898373008508f5e9223eda0
David Bernard <david.bernard.31@gmail.com>
2014-08-15 16:57:40 +0200

    display game in 16:9 viewport

--- src/vdrones/Main.java
+++ src/vdrones/Main.java
@@ -3,6 +3,7 @@ package vdrones;
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.Camera;
 
 public class Main {
 	private static boolean assertionsEnabled;
@@ -18,7 +19,8 @@ public class Main {
 			throw new RuntimeException("Assertions must be enabled (vm args -ea");
 		}
 		SimpleApplication app = Injectors.find().getInstance(SimpleApplication.class);
-		setDebug(app, true);
+		setDebug(app, false);
+		setAspectRatio(app, 16, 9);
 	}
 
 	static public void setDebug(SimpleApplication app, boolean v) {
@@ -27,6 +29,29 @@ public class Main {
 			app.getInputManager().setCursorVisible(v);
 			app.getViewPort().setBackgroundColor(v? ColorRGBA.Pink : ColorRGBA.White);
 			//Display.setResizable(v);
+			return true;
+		});
+	}
+
+	static public void setAspectRatio(SimpleApplication app, float w, float h) {
+		app.enqueue(() -> {
+			Camera cam = app.getCamera();
+			//cam.resize(w, h, true);
+			float ratio = (h * cam.getWidth()) / (w * cam.getHeight());
+			if (ratio < 1.0) {
+				float margin = (1f - ratio) * 0.5f;
+				System.out.printf("ratio %s\n", ratio);
+				float frustumW = cam.getFrustumRight(); //cam.getFrustumRight() / ratio;
+				float frustumH = cam.getFrustumTop() / ratio; //cam.getFrustumTop() / ratio;
+				//cam.resize(cam.getWidth(), (int)(cam.getHeight() * 0.5), true);
+				System.out.printf("frustum bottom %s top %s right %s left %s\n", cam.getFrustumBottom(), cam.getFrustumTop(), cam.getFrustumRight(), cam.getFrustumLeft() );
+				cam.setViewPort(0f, 1f, margin,  1 - margin);
+				cam.setFrustum(cam.getFrustumNear(), cam.getFrustumFar(), -frustumW, frustumW, frustumH, -frustumH);
+				System.out.printf("frustum bottom %s top %s right %s left %s\n", cam.getFrustumBottom(), cam.getFrustumTop(), cam.getFrustumRight(), cam.getFrustumLeft() );
+			}
+			Camera cam2 = app.getGuiViewPort().getCamera();
+			cam2.setViewPort(cam.getViewPortLeft(), cam.getViewPortRight(), cam.getViewPortBottom(), cam.getViewPortTop());
+			//cam2.setFrustum(cam.getViewPortLeft(), cam.getViewPortRight(), cam.getViewPortBottom(), cam.getViewPortTop());
 			return true;
 		});
 	}
