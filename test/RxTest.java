2c7c32c344fe57520014ef5b177e7ca4d88a6830
David Bernard <david.bernard.31@gmail.com>
2014-08-16 17:09:23 +0200

    clean

--- test/RxTest.java
+++ test/RxTest.java
@@ -1,13 +1,7 @@
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
 import org.junit.Test;
-
 import rx.Observable;
-import rx.Observer;
 import rx.subjects.BehaviorSubject;
-import rx.subjects.PublishSubject;
-import rx.subjects.Subject;
+import rx_ext.ObserverPrint;
 
 public class RxTest {
 
@@ -30,10 +24,10 @@ public class RxTest {
 		Observable<Float> shield = Observable.combineLatest(energy, shieldReq, (o1, o2) -> (o1 > energyShieldSpeed) ? o2 : 0f).distinctUntilChanged();
 		Observable.combineLatest(forward, shield, (o1, o2) -> (energyRegenSpeed - Math.abs(o1 * energyForwardSpeed) /*- Math.abs(o2 * energyShieldSpeed)*/)).subscribe(energyVelocity);
 		//energyVelocity.onNext(energyRegenSpeed);
-		forward.subscribe(new ObserverPrint<Float>("forward"));
-		shield.subscribe(new ObserverPrint<Float>("shield"));
-		energy.subscribe(new ObserverPrint<Float>("energy"));
-		energyVelocity.subscribe(new ObserverPrint<Float>("energyV"));
+		forward.subscribe(new ObserverPrint<>("forward"));
+		shield.subscribe(new ObserverPrint<>("shield"));
+		energy.subscribe(new ObserverPrint<>("energy"));
+		energyVelocity.subscribe(new ObserverPrint<>("energyV"));
 		dt.onNext(0.5f);
 		//energyVelocity.onNext(1.0f);
 		dt.onNext(0.5f);
@@ -48,26 +42,5 @@ public class RxTest {
 		//energy.subscribe( (v) -> energy.onNext(v + 2)); infinite loop
 		//energy.map(v -> )
 		//energy.onNext(44);
-	}
-	
-	@RequiredArgsConstructor
-	static class ObserverPrint<T> implements Observer<T> {
-		 private final String name;
-		
-		@Override
-		public void onCompleted() {
-			System.out.printf("%s completed\n", name);
-		}
-
-		@Override
-		public void onError(Throwable e) {
-			System.out.printf("%s error : %s\n", name, e);
-		}
-
-		@Override
-		public void onNext(T t) {
-			System.out.printf("%s value : %s\n", name, t);
-		}
-		
 	}
 }
