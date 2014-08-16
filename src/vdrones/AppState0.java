212316ad18a7b65e31a1e918b95b5b60a35a6295
David Bernard <david.bernard.31@gmail.com>
2014-08-16 09:42:14 +0200

    AppState0: catch+log exception

--- src/vdrones/AppState0.java
+++ src/vdrones/AppState0.java
@@ -14,58 +14,66 @@ import com.jme3.app.state.AppStateManager;
  */
 @Slf4j
 abstract public class AppState0 extends AbstractAppState {
-    protected Injector injector;
-    protected SimpleApplication app;
+	protected Injector injector;
+	protected SimpleApplication app;
 
-    @Override
-    public final void initialize(AppStateManager stateManager, Application app) {
-        super.initialize(stateManager, app);
-        injector = Injectors.find();
-        this.app = (SimpleApplication)app;
-        initialized = true;
-        doInitialize();
-        if( isEnabled() ) {
-            doEnable();
-        }
-    }
+	@Override
+	public final void initialize(AppStateManager stateManager, Application app) {
+		super.initialize(stateManager, app);
+		injector = Injectors.find();
+		this.app = (SimpleApplication)app;
+		initialized = true;
+		doInitialize();
+		if( isEnabled() ) {
+			doEnable();
+		}
+	}
 
-    @Override
-    public final void setEnabled( boolean enabled ) {
-        if( isEnabled() == enabled )
-            return;
-        super.setEnabled(enabled);
-        if( !isInitialized() )
-            return;
-        if( enabled ) {
-            log.trace("enable():" + this);
-            doEnable();
-        } else {
-            log.trace("disable():" + this);
-            doDisable();
-        }
-    }
+	@Override
+	public final void setEnabled( boolean enabled ) {
+		if( isEnabled() == enabled )
+			return;
+		super.setEnabled(enabled);
+		if( !isInitialized() )
+			return;
+		try {
+			if( enabled ) {
+				log.trace("enable():" + this);
+				doEnable();
+			} else {
+				log.trace("disable():" + this);
+				doDisable();
+			}
+		} catch(Exception exc) {
+			log.warn("failed to setEnabled(" + enabled + ")", exc);
+		}
+	}
 
-    @Override
-    public final void update(float tpf) {
-    	if (isEnabled()){
-    		doUpdate(tpf);
-    	}
-    };
+	@Override
+	public final void update(float tpf) {
+		if (isEnabled()){
+			doUpdate(tpf);
+		}
+	};
 
 
 	protected void doInitialize(){}
-    protected void doEnable(){};
-    protected void doUpdate(float tpf) {}
-    protected void doDisable(){};
-    protected void doDispose(){};
+	protected void doEnable(){};
+	protected void doUpdate(float tpf) {}
+	protected void doDisable(){};
+	protected void doDispose(){};
 
-    @Override
-    public final void cleanup() {
-        if( isEnabled() ) {
-            doDisable();
-        }
-        doDispose();
-        initialized = false;
-    }
+	@Override
+	public final void cleanup() {
+		setEnabled(false);
+		try {
+			if (isInitialized()) {
+				doDispose();
+			}
+		}catch (Exception exc) {
+			log.warn("failed to dispose", exc);
+		}
+		initialized = false;
+	}
 
 }
